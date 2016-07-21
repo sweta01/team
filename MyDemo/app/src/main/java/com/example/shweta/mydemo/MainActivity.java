4950ada243a63ccb793facfaaa55d1c285706b71
Sweta <sweta.rani@indiamart.com>
2016-07-21 17:54:15 +0530

    implement if else for switch

--- MyDemo/app/src/main/java/com/example/shweta/mydemo/MainActivity.java
+++ MyDemo/app/src/main/java/com/example/shweta/mydemo/MainActivity.java
@@ -18,7 +18,7 @@ import java.util.HashSet;
 public class MainActivity extends AppCompatActivity {
     private AdView mAdView;
     private Button btnFullscreenAd;
-    String str_duplicate[]={"1","2","2","3","5"};
+    String str_duplicate[] = {"1", "2", "2", "3", "5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
 //        find Duplicate Element in Array
         findDuplicateElement(str_duplicate);
         findDuplicateElementByHashset(str_duplicate);
+        switchCase(20);
+        forLoop();
+        lebelForLoop();
+        breakStatement();
+        fibonacci();
     }
 
     @Override
@@ -64,21 +69,91 @@ public class MainActivity extends AppCompatActivity {
         }
         super.onDestroy();
     }
-    public void findDuplicateElement(String str_duplicate[]){
-        for(int i=0;i<str_duplicate.length;i++){
-            for(int j=0;j<str_duplicate.length;j++){
-                if(str_duplicate[i].equals(str_duplicate[j])&& i!=j){
-                    System.out.println("Duplicate Element "+ str_duplicate[i]);
+
+    public void findDuplicateElement(String str_duplicate[]) {
+        for (int i = 0; i < str_duplicate.length; i++) {
+            for (int j = 0; j < str_duplicate.length; j++) {
+                if (str_duplicate[i].equals(str_duplicate[j]) && i != j) {
+                    System.out.println("Duplicate Element " + str_duplicate[i]);
                 }
             }
         }
     }
-    public void  findDuplicateElementByHashset(String str_duplicate[]){
-        HashSet <String>hashset=new HashSet<String>();
-        for(String str:str_duplicate){
-            if(!hashset.add(str)){
-                System.out.println("Duplicate Element By Using Hashset"+str);
+
+    public void findDuplicateElementByHashset(String str_duplicate[]) {
+        HashSet<String> hashset = new HashSet<String>();
+        for (String str : str_duplicate) {
+            if (!hashset.add(str)) {
+                System.out.println("Duplicate Element By Using Hashset" + str);
             }
+        }
+
+
+    }
+
+    public void switchCase(int number) {
+        switch (number) {
+            case 10:
+                System.out.println("Switch" + "10");
+                break;
+            case 20:
+                System.out.println("Switch" + "20");
+
+            case 30:
+                System.out.println("Switch" + "30");
+                break;
+            default:
+                System.out.println("did not match any one of them");
+        }
+    }
+
+    public void forLoop() {
+        for (int i = 1; i < 10; i++) {
+            System.out.println("For Loop" + i);
+        }
+
+    }
+
+    public void lebelForLoop() {
+        aa:
+        for (int i = 1; i <= 3; i++) {
+            bb:
+            for (int j = 1; j <= 3; j++) {
+                if (i == 2 && j == 2) {
+                    break aa;
+                }
+                System.out.println("Lebel For Loop" + i + "" + j);
+            }
+        }
+    }
+
+    public void whileLoop() {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("while loop" + i);
+            i++;
+        }
+    }
+
+    public void breakStatement() {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println("Break :" + i);
+        }
+    }
+
+    public void fibonacci() {
+
+        int first = 0, second = 1, third;
+        System.out.print(first+" "+second);
+        for (int i = 1; i <= 10; i++) {
+            third=first+second;
+            System.out.print(" "+third);
+            first=second;
+            second=third;
+
         }
 
 
