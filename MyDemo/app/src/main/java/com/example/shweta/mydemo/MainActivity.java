ee9e4dbe85b24b3bf8e4809d82a13d688c7aac3f
Sweta <sweta.rani@indiamart.com>
2016-09-06 17:57:14 +0530

    removing idea folder

--- MyDemo/app/src/main/java/com/example/shweta/mydemo/MainActivity.java
+++ MyDemo/app/src/main/java/com/example/shweta/mydemo/MainActivity.java
@@ -1,22 +1,13 @@
 package com.example.shweta.mydemo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
 import java.util.HashSet;
 
-public class MainActivity extends AppCompatActivity {
-    private AdView mAdView;
+public class MainActivity extends Activity {
+//    private AdView mAdView;
     private Button btnFullscreenAd;
     String str_duplicate[] = {"1", "2", "2", "3", "5"};
     static int firstValue=60;
@@ -25,11 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice("E8BCEFC40068C63BD18F1F9DCA661500")
-                .build();
-        mAdView.loadAd(adRequest);
         btnFullscreenAd = (Button) findViewById(R.id.btn_fullscreen_ad);
         btnFullscreenAd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,29 +37,7 @@ public class MainActivity extends AppCompatActivity {
         sum(2,3,4);
     }
 
-    @Override
-    public void onPause() {
-        if (mAdView != null) {
-            mAdView.pause();
-        }
-        super.onPause();
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mAdView != null) {
-            mAdView.resume();
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        if (mAdView != null) {
-            mAdView.destroy();
-        }
-        super.onDestroy();
-    }
 
     public void findDuplicateElement(String str_duplicate[]) {
         for (int i = 0; i < str_duplicate.length; i++) {
